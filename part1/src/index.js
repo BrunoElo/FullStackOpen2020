9557c3a24a71b2dc6f17f500946e0158a2037b99
bruno <elochux@gmail.com>
2021-01-25 05:20:39 +0100

    Exercise 1.8: unicafe step3 completed

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,18 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  // corresponding states for each button
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
+const Statistics = (props) => {
+  console.log(props);
+  const { good, neutral, bad } = props;
   return (
     <>
-      <h2>Give Feedback</h2>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
       <h3>Statistics</h3>
       <>
         <p>good:{good}</p>
@@ -22,6 +15,28 @@ const App = () => {
         <p>average feedback:{(good + neutral + bad) / 3}</p>
         <p>positive feedback:{(good / (good + neutral + bad)) * 100 || 0}</p>
       </>
+    </>
+  );
+};
+
+const App = () => {
+  // corresponding states for each button
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // reference function methods to handle events
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h2>Give Feedback</h2>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
