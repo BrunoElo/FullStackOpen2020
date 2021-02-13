9db983eaa902b244d2576534a98ffc26dec483c4
bruno <elochux@gmail.com>
2021-02-13 16:26:17 +0100

    Exercise 2.9: The Phonebook step4 completed

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "09044994499" },
+    { name: "Bell Zalo", number: "09044994498" },
+    { name: "Mylo Drese", number: "09044994439" },
+    { name: "Lasa Feri", number: "09044994429" },
+    { name: "Sara Goli", number: "09044994419" },
+  ]);
+
+  const [searchList] = useState(persons);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -24,8 +34,18 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchNameChange = (event) => {
+    setSearchName(event.target.value);
+    const result = searchList.filter((person) =>
+      person.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setPersons(result);
+  };
+
   return (
     <div>
+      Search:
+      <input value={searchName} onChange={handleSearchNameChange} />
       <h2>Phonebook</h2>
       <form>
         <div>
