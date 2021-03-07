417187d8c78acc6615cab5f27dffa9d6f0357e20
bruno <elochux@gmail.com>
2021-03-07 17:50:53 +0100

    modified part2 phonebook exercise

--- part2/thephonebook/src/components/App.js
+++ part2/thephonebook/src/components/App.js
@@ -1,101 +1,9 @@
 import React, { useEffect, useState } from "react";
 import apiService from "../services/api-service";
-
-const Search = ({ searchName, handleSearchNameChange }) => {
-  return (
-    <div>
-      Search:
-      <input value={searchName} onChange={handleSearchNameChange} />
-    </div>
-  );
-};
-
-const Form = (props) => {
-  const {
-    newName,
-    newNumber,
-    handleNameChange,
-    handleNumberChange,
-    addName,
-  } = props;
-  return (
-    <form>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-        <br />
-        number: <input value={newNumber} onChange={handleNumberChange} />
-      </div>
-      <div>
-        <button type="submit" onClick={addName}>
-          add
-        </button>
-      </div>
-    </form>
-  );
-};
-
-const List = ({ persons, setPersons, setNotify }) => {
-  console.log(persons);
-  return (
-    <ul>
-      {persons.map((person) => {
-        return (
-          <Persons
-            persons={persons}
-            key={person.id}
-            person={person}
-            setPersons={setPersons}
-            setNotify={setNotify}
-          />
-        );
-      })}
-    </ul>
-  );
-};
-
-const Persons = ({ person, setPersons, persons, setNotify }) => {
-  const remove = (id, name) => () => {
-    if (window.confirm(`Are you sure you want to delete ${name}?`)) {
-      apiService
-        .deleteItem(id)
-        .then((response) => {
-          const newPersons = persons.filter((person) => person.id !== id);
-          setPersons(newPersons);
-          setNotify(`${name} successfully deleted`);
-          setTimeout(() => setNotify(null), 4000);
-        })
-        .catch((error) => {
-          setNotify(`Sorry, ${name} may have been deleted from the database`);
-          setTimeout(() => setNotify(null), 4000);
-          setPersons(persons.filter((person) => person.id !== id));
-        });
-    }
-  };
-
-  return (
-    <li>
-      {person.name} {person.number}{" "}
-      <button onClick={remove(person.id, person.name)}>Delete</button>
-    </li>
-  );
-};
-
-const Notification = ({ notify }) => {
-  if (!notify) {
-    return null;
-  } else if (notify.includes("Sorry")) {
-    return (
-      <div className="error-notification">
-        <p>{notify}</p>
-      </div>
-    );
-  }
-  return (
-    <div className="success-notification">
-      <p>{notify}</p>
-    </div>
-  );
-};
+import Notification from "./Notification";
+import Search from "./Search";
+import Form from "./Form";
+import List from "./List";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -104,7 +12,7 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchName, setSearchName] = useState("");
-  const [notify, setNotify] = useState("");
+  const [notify, setNotify] = useState(null);
 
   useEffect(() => {
     apiService.getAll().then((response) => {
@@ -112,6 +20,13 @@ const App = () => {
       setSearchList(response);
     });
   }, []);
+
+  const notifyUserWith = (message, type = "success-notification") => {
+    setNotify({ message, type });
+    setTimeout(() => {
+      setNotify(null);
+    }, 5000);
+  };
 
   const addName = (event) => {
     event.preventDefault();
@@ -134,26 +49,35 @@ const App = () => {
                 person.id !== entry.id ? person : response.data
               )
             );
-            setNotify(`${newName} successfully updated`);
-            setTimeout(() => setNotify(null), 4000);
+            notifyUserWith(
+              `${newName} successfully updated`,
+              "success-notification"
+            );
           })
           .catch((error) => {
             /*  alert(
               `Sorry, ${entry.name} may have been deleted from the database`
             ); */
-            setNotify(
-              `Sorry, ${entry.name} may have been deleted from the database`
+            notifyUserWith(
+              `${error.response.data.error}`,
+              "error-notification"
             );
-            setTimeout(() => setNotify(null), 4000);
-            setPersons(persons.filter((person) => person.id !== entry.id));
+            //setPersons(persons.filter((person) => person.id !== entry.id));
           });
       }
     } else {
-      apiService.create(payload).then((response) => {
-        setPersons(persons.concat(response));
-        setNotify(`${newName} successfully added`);
-        setTimeout(() => setNotify(null), 5000);
-      });
+      apiService
+        .create(payload)
+        .then((response) => {
+          setPersons(persons.concat(response));
+          notifyUserWith(
+            `${newName} successfully added`,
+            "success-notification"
+          );
+        })
+        .catch((error) => {
+          notifyUserWith(`${error.response.data.error}`, "error-notification");
+        });
     }
     setNewName("");
     setNewNumber("");
@@ -191,7 +115,11 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <List persons={persons} setPersons={setPersons} setNotify={setNotify} />
+      <List
+        persons={persons}
+        setPersons={setPersons}
+        notifyUserWith={notifyUserWith}
+      />
     </div>
   );
 };
