afdd3c8f1687bc922db93451e7d9732109523e44
bruno <elochux@gmail.com>
2021-03-07 08:54:12 +0100

    Exercises 3.19.-3.21. Deploying the database backend to production completed

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
@@ -119,7 +121,11 @@ app.put("/api/persons/:id", (request, response, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -127,7 +133,7 @@ app.put("/api/persons/:id", (request, response, next) => {
 });
 
 // Create a new item
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -145,9 +151,12 @@ app.post("/api/persons", (request, response) => {
     id: genrateId(),
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 
   /* persons = persons.concat(person);
   response.json(person); */
