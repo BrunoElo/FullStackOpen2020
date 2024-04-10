09cab2a9c6743ee8014ff7d6b94de8747464da98
BrunoElo <elochux@gmail.com>
2024-04-10 16:58:47 +0100

    Exercise 8.19 Books by genre, part 1 completed
    The filtering was done using state while letting the dom render what it needs to

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,37 +1,43 @@
 import {useQuery} from "@apollo/client";
 import {ALL_BOOKS} from "../queries";
+import {useState} from "react";
 
 const Books = (props) => {
-  const result = useQuery(ALL_BOOKS);
+    const [genre, setGenre] = useState(null)
+    const result = useQuery(ALL_BOOKS);
 
-  if (!props.show) {
-    return null
-  }
+    if (!props.show) {
+        return null
+    }
 
-  const books = result.data?.allBooks ?? []
+    const books = result.data?.allBooks ?? []
+    const genres = [...new Set(books.map(book => book.genres).flat())]
 
-  return (
-    <div>
-      <h2>books</h2>
+    return (
+        <div>
+            <h2>books</h2>
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+            <table>
+                <tbody>
+                <tr>
+                    <th></th>
+                    <th>author</th>
+                    <th>published</th>
+                </tr>
+                {books.map((a) => (
+                    (genre ? genre && a.genres.includes(genre) : true) && // 👏
+                    <tr key={a.title}>
+                        <td>{a.title}</td>
+                        <td>{a.author.name}</td>
+                        <td>{a.published}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            {genres.map(genre => (<button key={genre} onClick={() => setGenre(genre)}>{genre}</button>))}
+            <button onClick={() => setGenre(null)}>All genre</button>
+        </div>
+    )
 }
 
 export default Books
